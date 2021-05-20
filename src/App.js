fc64eaa360c546f6f1fd70af1cdc3b24522f26e3
deepshah1309 <deepshah1309@gmail.com>
2021-05-20 20:34:00 +0530

    Adding Main Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
+import Search from "./Components/Search/Search";
+import Main from "./Components/Main/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+          <NavBar/>
+          <Search/>
+          <Main/>
+          <Footer/>
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,18 @@
+import Card from "../Card/Card";
+import "./Main.scss";
+const Main=()=>{
+    return (
+        <div className="blog_display">
+
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                
+                
+        </div>
+    )
+}
+export default Main;
