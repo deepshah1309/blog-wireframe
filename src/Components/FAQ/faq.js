5e86990a77218defd2e7fb8abc9782551c4cfcc0
deepshah1309 <deepshah1309@gmail.com>
2021-05-23 00:07:57 +0530

    Box

--- src/Components/FAQ/faq.js
+++ src/Components/FAQ/faq.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React,{useState} from "react";
 import Question from "./Question";
 import Search from "../Search/Search";
 import "./faq.scss";
+
 
 const data = [
   {
@@ -12,7 +13,9 @@ const data = [
 
 var d = new Array(5);
 
-function faq() {
+function Faq() {
+  const [obj,setObj]=useState({email:"",Query:""});
+  console.log(obj);
   return (
     <>
       <Search title={"FAQ"} />
@@ -23,9 +26,22 @@ function faq() {
         <Question title={data[0].title} info={data[0].info} />
         <Question title={data[0].title} info={data[0].info} />
         <Question title={data[0].title} info={data[0].info} />
+
+      </div>
+      <div className="FAQ_creation">
+        <div className="TitleBox"><b>Ask Me</b></div>
+        <div className="FAQ_inputbox">
+            <div className="emailbox">
+              <div className="part1"><label htmlFor="email">Email</label></div>
+              <div className="part2"><input type="email" value={obj.email} onChange={(e)=>{setObj({...obj,[e.target.name]:e.target.value})}} id="email" name="email" placeholder="Enter email"></input></div>
+            </div>
+            <div className="querybox"><textarea placeholder="Ask Your Queries here" rows="20" name="Query" value={obj.Query} onChange={(e)=>{setObj({...obj,[e.target.name]:e.target.value})}}></textarea></div>
+            <div className="faqsubmit"><button className="FAQ_POST">POST</button></div>
+        </div>
+
       </div>
     </>
   );
 }
 
-export default faq;
+export default Faq;
