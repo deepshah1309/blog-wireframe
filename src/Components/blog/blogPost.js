c76fa117aadd5b05ccbfc7f9e4809e18145cac58
deepshah1309 <deepshah1309@gmail.com>
2021-05-23 19:57:35 +0530

    Changes to Blog Section

--- src/Components/blog/blogPost.js
+++ src/Components/blog/blogPost.js
@@ -1,7 +1,44 @@
 import React from "react";
+import "./Blogpost.scss"
 
 function blogPost() {
-  return <div>BlogPost</div>;
+  return <div>
+    
+    <div>
+    <section class="content">
+    
+  
+  
+    <div className="ImageSection">
+      
+      
+    </div>
+
+  <div>
+  <p>
+    Etiam ultrices, augue in eleifend commodo, ex mi vulputate massa, ut convallis elit mi at quam. Etiam aliquet non massa in auctor. Nunc sagittis ac felis sit amet cursus. Suspendisse dictum, purus in accumsan rutrum, nibh urna gravida purus, sit amet aliquam eros est eu ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam efficitur mauris leo, at consectetur magna malesuada vitae. Morbi consectetur eget ipsum vitae cursus. Praesent lacinia ultricies tempor. Quisque dictum feugiat est, in gravida tortor tincidunt sed. Aliquam erat volutpat. Etiam dictum suscipit dolor. Nam dictum cursus diam non suscipit.
+  </p>
+  <p>
+    Donec velit sapien, finibus quis purus nec, ullamcorper dictum enim. Integer vitae ullamcorper enim. Aenean commodo lorem et augue accumsan tincidunt. Pellentesque sapien justo, feugiat in diam vitae, efficitur vehicula ligula. Morbi in tellus ut mauris pulvinar lacinia. Curabitur ac maximus lacus, ut lacinia nunc. Nunc pharetra nibh vitae nunc ornare, sed tincidunt neque aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam efficitur ut turpis sed porta. Duis commodo tincidunt ipsum, sit amet egestas dolor bibendum vel. Integer nec magna turpis. Curabitur aliquet lobortis odio, nec facilisis ipsum.
+  </p>
+  <p>
+    Donec scelerisque aliquet tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean dictum urna id placerat sagittis. Vestibulum et interdum urna, pulvinar volutpat eros. Fusce consequat rhoncus nisl, vel malesuada nulla lacinia sit amet. Nulla in aliquet magna, eu malesuada orci. Nullam ligula lacus, sollicitudin ac metus a, condimentum efficitur ligula. Aliquam posuere, magna eget fringilla ornare, purus tortor ullamcorper eros, ut fringilla justo dolor eu est. Fusce sagittis lacus et finibus tincidunt. 
+  </p>
+  <p>
+       Aenean et egestas erat. Nullam aliquam tortor quis nulla feugiat, ac aliquet neque euismod. Aenean auctor risus sed nibh feugiat, vel volutpat elit finibus. Vestibulum vulputate leo vel ex porttitor, at sodales purus mattis. Suspendisse vulputate nisl id erat ultricies, commodo tempor lectus mattis. Sed turpis tortor, tincidunt quis viverra in, laoreet at diam. Donec varius turpis nisi, vitae blandit felis gravida eget. Aenean vitae molestie leo. In sapien mi, ultrices a arcu in, sodales lobortis elit. Pellentesque sit amet nibh at ex iaculis venenatis. Quisque sed orci vel lorem congue sodales.
+  </p>
+  </div>
+  
+</section>
+<div class="side-tab1">
+  Left
+</div>
+ 
+    <div class="side-tab2">
+  Right
+</div>
+  </div>
+  </div>
 }
 
 export default blogPost;
